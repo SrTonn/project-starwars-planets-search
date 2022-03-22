3ab2f0bb76268a7b13f472f574bac770754fe852
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-22 15:37:59 -0300

    fetch dos planets configurado

--- src/Context/Provider.jsx
+++ src/Context/Provider.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 export default function Provider({ children }) {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const { results } = await data.json();
+      setPlanets(results);
+    })();
+  }, []);
+
   const context = {
+    planets,
   };
 
   return (
