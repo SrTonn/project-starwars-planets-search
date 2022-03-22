bb420ec7c9b9bfe4cee61a5ac67baf6f49eea15c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-22 17:14:21 -0300

    componente Table movido

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import Context from '../Context/Context';
+import Context from '../../Context/Context';
 
 export default function Table() {
   const { planets } = useContext(Context);
