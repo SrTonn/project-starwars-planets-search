43471179f47f6a0db167a132d884e580383c44cb
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-22 19:37:27 -0300

    refatoracao do componente Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { uid } from 'uid';
 import Context from '../../Context/Context';
 import removeUnderline from '../../helpers/formatText';
 
@@ -11,26 +12,15 @@ export default function Table() {
         <tr>
           {planets.length > 0 && Object.keys(planets[0]).map((th) => {
             if (th === 'residents') return;
-            return <th key={ th }>{removeUnderline(th)}</th>;
+            return <th key={ uid() }>{removeUnderline(th)}</th>;
           })}
         </tr>
       </thead>
       <tbody>
         {filteredPlanets.map((obj) => (
           <tr key={ obj.name }>
-            <td>{obj.name}</td>
-            <td>{obj.rotation_period}</td>
-            <td>{obj.orbital_period}</td>
-            <td>{obj.diameter}</td>
-            <td>{obj.climate}</td>
-            <td>{obj.gravity}</td>
-            <td>{obj.terrain}</td>
-            <td>{obj.surface_water}</td>
-            <td>{obj.population}</td>
-            <td>{obj.film}</td>
-            <td>{obj.created}</td>
-            <td>{obj.edited}</td>
-            <td>{obj.url}</td>
+            {Object.keys(planets[0])
+              .map((content) => (<td key={ uid() }>{obj[content]}</td>))}
           </tr>
         ))}
       </tbody>
