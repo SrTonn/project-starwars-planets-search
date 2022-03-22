ee77d7836837d46e7b6e7ac8d4847f2207a89cc3
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-22 18:01:00 -0300

    requisito 02 concluido

--- src/Context/Provider.jsx
+++ src/Context/Provider.jsx
@@ -4,17 +4,30 @@ import Context from './Context';
 
 export default function Provider({ children }) {
   const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
 
   useEffect(() => {
     (async () => {
       const data = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
       const { results } = await data.json();
       setPlanets(results);
+      setFilteredPlanets(results);
     })();
   }, []);
 
+  const handleChange = ({ target }) => {
+    const { value } = target;
+
+    if (value) {
+      setFilteredPlanets(planets.filter(({ name }) => name.includes(value)));
+    } else {
+      setFilteredPlanets(planets);
+    }
+  };
+
   const context = {
-    planets,
+    planets: filteredPlanets,
+    handleChange,
   };
 
   return (
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../Context/Context';
 import Table from '../Table/Table';
 
 export default function Main() {
+  const { handleChange } = useContext(Context);
   return (
-    <Table />
+    <>
+      <h1>Projeto Star Wars</h1>
+      <input data-testid="name-filter" type="text" onChange={ (e) => handleChange(e) } />
+      <Table />
+    </>
   );
 }
